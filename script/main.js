5d1208f412cda78fe09f92a663833e091ae13131
VidKein <landdru@ukr.net>
2024-11-03 18:21:47 +0100

    kalendarg1

--- script/main.js
+++ script/main.js
@@ -52,6 +52,141 @@ function onLocationError(e) {alert(e.message);}
 map.on('locationerror', onLocationError);
 
 /*------------------------------------------*/
+/*Календарь*/
+class Cal {
+    constructor(divId) {
+        //Сохраняем идентификатор div
+        this.divId = divId;
+        // Дни недели с понедельника
+        this.DaysOfWeek = [
+            'Пн',
+            'Вт',
+            'Ср',
+            'Чтв',
+            'Птн',
+            'Суб',
+            'Вск'
+        ];
+        // Месяцы начиная с января
+        this.Months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+        //Устанавливаем текущий месяц, год
+        var d = new Date();
+        this.currMonth = d.getMonth();
+        this.currYear = d.getFullYear();
+        this.currDay = d.getDate();
+    }
+    // Переход к следующему месяцу
+    nextMonth() {
+        if (this.currMonth == 11) {
+            this.currMonth = 0;
+            this.currYear = this.currYear + 1;
+        }
+        else {
+            this.currMonth = this.currMonth + 1;
+        }
+        this.showcurr();
+    }
+    // Переход к предыдущему месяцу
+    previousMonth() {
+        if (this.currMonth == 0) {
+            this.currMonth = 11;
+            this.currYear = this.currYear - 1;
+        }
+        else {
+            this.currMonth = this.currMonth - 1;
+        }
+        this.showcurr();
+    }
+    // Показать текущий месяц
+    showcurr() {
+        this.showMonth(this.currYear, this.currMonth);
+    }
+    // Показать месяц (год, месяц)
+    showMonth(y, m) {
+        var d = new Date()
+            // Первый день недели в выбранном месяце 
+            , firstDayOfMonth = new Date(y, m, 7).getDay()
+            // Последний день выбранного месяца
+            , lastDateOfMonth = new Date(y, m + 1, 0).getDate()
+            // Последний день предыдущего месяца
+            , lastDayOfLastMonth = m == 0 ? new Date(y - 1, 11, 0).getDate() : new Date(y, m, 0).getDate();
+        var html = '<table>';
+        // Запись выбранного месяца и года
+        html += '<thead><tr>';
+        html += '<td colspan="7">' + this.Months[m] + ' ' + y + '</td>';
+        html += '</tr></thead>';
+        // заголовок дней недели
+        html += '<tr class="days">';
+        for (var i = 0; i < this.DaysOfWeek.length; i++) {
+            html += '<td>' + this.DaysOfWeek[i] + '</td>';
+        }
+        html += '</tr>';
+        // Записываем дни
+        var i = 1;
+        do {
+            var dow = new Date(y, m, i).getDay();
+            // Начать новую строку в понедельник
+            if (dow == 1) {
+                html += '<tr>';
+            }
+
+            // Если первый день недели не понедельник показать последние дни предыдущего месяца
+            else if (i == 1) {
+                html += '<tr>';
+                var k = lastDayOfLastMonth - firstDayOfMonth + 1;
+                for (var j = 0; j < firstDayOfMonth; j++) {
+                    html += '<td class="not-current">' + k + '</td>';
+                    k++;
+                }
+            }
+            // Записываем текущий день в цикл
+            var chk = new Date();
+            var chkY = chk.getFullYear();
+            var chkM = chk.getMonth();
+            if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay) {
+                html += '<td class="today">' + i + '</td>';
+            } else {
+                html += '<td class="normal">' + i + '</td>';
+            }
+            // закрыть строку в воскресенье
+            if (dow == 0) {
+                html += '</tr>';
+            }
+
+            // Если последний день месяца не воскресенье, показать первые дни следующего месяца
+            else if (i == lastDateOfMonth) {
+                var k = 1;
+                for (dow; dow < 7; dow++) {
+                    html += '<td class="not-current">' + k + '</td>';
+                    k++;
+                }
+            }
+            i++;
+        } while (i <= lastDateOfMonth);
+        // Конец таблицы
+        html += '</table>';
+        // Записываем HTML в div
+        document.getElementById(this.divId).innerHTML = html;
+    }
+}
+  // При загрузке окна
+  window.onload = function() {
+    // Начать календарь
+    var c = new Cal("divCal");			
+    c.showcurr();
+    // Привязываем кнопки «Следующий» и «Предыдущий»
+    getId('btnNext').onclick = function() {
+      c.nextMonth();
+    };
+    getId('btnPrev').onclick = function() {
+      c.previousMonth();
+    };
+  }
+  // Получить элемент по id
+  function getId(id) {
+    return document.getElementById(id);
+  }
+/*------------------------------------------*/
 //Извлекаем информацию о точках
 let xhr = new XMLHttpRequest();
 //запрос на извлечение
@@ -79,10 +214,8 @@ function createMarker(name, position, systemCoordinates, vycka) {
         //Подключение маркера с конвертацией JTSKtoWGS84
         var marker = L.marker([wgs.lat,wgs.lon]).addTo(map);
         marker.bindPopup("<b>"+name+"</b><br>vycka: "+vycka+" m.");
-        console.log(name, wgs, systemCoordinates, vycka); 
     } else {
         //Подключение маркера с WGS84
-        console.log(name, position, systemCoordinates, vycka);  
     }
 }
 
